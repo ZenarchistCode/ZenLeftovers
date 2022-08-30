ebc067263077cdb8ba5c47b763d21ddd60c62cac
Zenarchist <103560182+ZenarchistCode@users.noreply.github.com>
2022-08-30 10:42:48 +1000

    Update

--- scripts/3_game/LeftoversConfig.c
+++ scripts/3_game/LeftoversConfig.c
@@ -15,71 +15,73 @@ class LeftoversConfig
 		if (GetGame().IsServer()) 
 		{
 			if (FileExist(zenModFolder + zenConfigName))
-			{ // If config exists, load file
+			{	// If config exists, load file
 				JsonFileLoader<LeftoversConfig>.JsonLoadFile(zenModFolder + zenConfigName, this);
 
-				// Backup old version of json before replacing it
+				// If version mismatch, backup old version of json before replacing it
 				if (ConfigVersion != "2")
 				{
 					JsonFileLoader<LeftoversConfig>.JsonSaveFile(zenModFolder + zenConfigName + "_old", this);
 					ConfigVersion = "2";
-					Save();
+				}
+				else
+				{
+					// Config exists and version matches, stop here.
+					return;
 				}
 			}
-			else 
-			{ // Config file does not exist, create default file
-				// Save vanilla items
-				LeftoverItems.Insert(new LeftoverItem("SodaCan_Pipsi", "Empty_SodaCan_Pipsi", 0));
-				LeftoverItems.Insert(new LeftoverItem("SodaCan_Cola", "Empty_SodaCan_Cola", 0));
-				LeftoverItems.Insert(new LeftoverItem("SodaCan_Spite", "Empty_SodaCan_Spite", 0));
-				LeftoverItems.Insert(new LeftoverItem("SodaCan_Kvass", "Empty_SodaCan_Kvass", 0));
-				LeftoverItems.Insert(new LeftoverItem("SodaCan_Fronta", "Empty_SodaCan_Fronta", 0));
-				LeftoverItems.Insert(new LeftoverItem("BoxCerealCrunchin", "Empty_BoxCerealCrunchin", 1));
-				LeftoverItems.Insert(new LeftoverItem("Rice", "Empty_Rice", 1));
-				LeftoverItems.Insert(new LeftoverItem("PowderedMilk", "Empty_PowderedMilk", 1));
-				LeftoverItems.Insert(new LeftoverItem("Marmalade", "Empty_Marmalade", 0));
-				LeftoverItems.Insert(new LeftoverItem("Honey", "Empty_Honey", 0));
-				LeftoverItems.Insert(new LeftoverItem("Zagorky", "Empty_Zagorky", 1));
-				LeftoverItems.Insert(new LeftoverItem("ZagorkyChocolate", "Empty_ZagorkyChocolate", 1));
-				LeftoverItems.Insert(new LeftoverItem("ZagorkyPeanuts", "Empty_ZagorkyPeanuts", 1));
-				LeftoverItems.Insert(new LeftoverItem("SaltySticks", "Empty_SaltySticks", 1));
-				LeftoverItems.Insert(new LeftoverItem("Crackers", "Empty_Crackers", 1));
-				LeftoverItems.Insert(new LeftoverItem("Chips", "Empty_Chips", 1));
-				LeftoverItems.Insert(new LeftoverItem("BakedBeansCan_Opened", "Empty_BakedBeansCan_Opened", 0));
-				LeftoverItems.Insert(new LeftoverItem("PeachesCan_Opened", "Empty_PeachesCan_Opened", 0));
-				LeftoverItems.Insert(new LeftoverItem("TacticalBaconCan_Opened", "Empty_TacticalBaconCan_Opened", 0));
-				LeftoverItems.Insert(new LeftoverItem("SpaghettiCan_Opened", "Empty_SpaghettiCan_Opened", 0));
-				LeftoverItems.Insert(new LeftoverItem("UnknownFoodCan_Opened", "Empty_UnknownFoodCan_Opened", 0));
-				LeftoverItems.Insert(new LeftoverItem("SardinesCan_Opened", "Empty_SardinesCan_Opened", 0));
-				LeftoverItems.Insert(new LeftoverItem("TunaCan_Opened", "Empty_TunaCan_Opened", 0));
-				LeftoverItems.Insert(new LeftoverItem("DogFoodCan_Opened", "Empty_DogFoodCan_Opened", 0));
-				LeftoverItems.Insert(new LeftoverItem("CatFoodCan_Opened", "Empty_CatFoodCan_Opened", 0));
-				LeftoverItems.Insert(new LeftoverItem("PorkCan_Opened", "Empty_PorkCan_Opened", 0));
-				LeftoverItems.Insert(new LeftoverItem("Lunchmeat_Opened", "Empty_Lunchmeat_Opened", 0));
-				LeftoverItems.Insert(new LeftoverItem("Pate_Opened", "Empty_Pate_Opened", 0));
-				LeftoverItems.Insert(new LeftoverItem("Pajka_Opened", "Empty_Pate_Opened", 0));
-				LeftoverItems.Insert(new LeftoverItem("BrisketSpread_Opened", "Empty_BrisketSpread_Opened", 0));
-				LeftoverItems.Insert(new LeftoverItem("BloodBagIV", "Used_BloodBagIV", 1, true));
-				LeftoverItems.Insert(new LeftoverItem("SalineBagIV", "Used_SalineBagIV", 1, true));
-				LeftoverItems.Insert(new LeftoverItem("Morphine", "Used_Morphine", 1, true));
-				LeftoverItems.Insert(new LeftoverItem("Epinephrine", "Used_Epinephrine", 1, true));
-				LeftoverItems.Insert(new LeftoverItem("AntiChemInjector", "Used_AntiChemInjector", 1, true));
-				// Example modded items
-				LeftoverItems.Insert(new LeftoverItem("MassMREPB", "MassMREPB", 1));
-				LeftoverItems.Insert(new LeftoverItem("MassMREJelly", "MassMREJelly", 1));
-				LeftoverItems.Insert(new LeftoverItem("MassMREGrapeJelly", "MassMREGrapeJelly", 1));
-				LeftoverItems.Insert(new LeftoverItem("MassMRETobasco", "MassMRETobasco", 1));
-				LeftoverItems.Insert(new LeftoverItem("MassMREHotCheeseSpread", "MassMREHotCheeseSpread", 1));
-				LeftoverItems.Insert(new LeftoverItem("MassMREDBBPudding", "MassMREDBBPudding", 1));
-				LeftoverItems.Insert(new LeftoverItem("MassMREDBBCobbler", "MassMREDBBCobbler", 1));
-				LeftoverItems.Insert(new LeftoverItem("MassMREMCSpaghetti", "MassMREMCSpaghetti", 1));
-				LeftoverItems.Insert(new LeftoverItem("MassMREMCBeefTaco", "MassMREMCBeefTaco", 1));
-				LeftoverItems.Insert(new LeftoverItem("MassMREMCChilliMac", "MassMREMCChilliMac", 1));
-				LeftoverItems.Insert(new LeftoverItem("MassMREMCBeefStew", "MassMREMCBeefStew", 1));
-				LeftoverItems.Insert(new LeftoverItem("MassMRETortillaW", "MassMRETortillaW", 1));
-				// Save config
-				Save();
-			}
+			
+			// Config file does not exist, create default file
+			LeftoverItems.Insert(new LeftoverItem("SodaCan_Pipsi", "Empty_SodaCan_Pipsi", 0));
+			LeftoverItems.Insert(new LeftoverItem("SodaCan_Cola", "Empty_SodaCan_Cola", 0));
+			LeftoverItems.Insert(new LeftoverItem("SodaCan_Spite", "Empty_SodaCan_Spite", 0));
+			LeftoverItems.Insert(new LeftoverItem("SodaCan_Kvass", "Empty_SodaCan_Kvass", 0));
+			LeftoverItems.Insert(new LeftoverItem("SodaCan_Fronta", "Empty_SodaCan_Fronta", 0));
+			LeftoverItems.Insert(new LeftoverItem("BoxCerealCrunchin", "Empty_BoxCerealCrunchin", 1));
+			LeftoverItems.Insert(new LeftoverItem("Rice", "Empty_Rice", 1));
+			LeftoverItems.Insert(new LeftoverItem("PowderedMilk", "Empty_PowderedMilk", 1));
+			LeftoverItems.Insert(new LeftoverItem("Marmalade", "Empty_Marmalade", 0));
+			LeftoverItems.Insert(new LeftoverItem("Honey", "Empty_Honey", 0));
+			LeftoverItems.Insert(new LeftoverItem("Zagorky", "Empty_Zagorky", 1));
+			LeftoverItems.Insert(new LeftoverItem("ZagorkyChocolate", "Empty_ZagorkyChocolate", 1));
+			LeftoverItems.Insert(new LeftoverItem("ZagorkyPeanuts", "Empty_ZagorkyPeanuts", 1));
+			LeftoverItems.Insert(new LeftoverItem("SaltySticks", "Empty_SaltySticks", 1));
+			LeftoverItems.Insert(new LeftoverItem("Crackers", "Empty_Crackers", 1));
+			LeftoverItems.Insert(new LeftoverItem("Chips", "Empty_Chips", 1));
+			LeftoverItems.Insert(new LeftoverItem("BakedBeansCan_Opened", "Empty_BakedBeansCan_Opened", 0));
+			LeftoverItems.Insert(new LeftoverItem("PeachesCan_Opened", "Empty_PeachesCan_Opened", 0));
+			LeftoverItems.Insert(new LeftoverItem("TacticalBaconCan_Opened", "Empty_TacticalBaconCan_Opened", 0));
+			LeftoverItems.Insert(new LeftoverItem("SpaghettiCan_Opened", "Empty_SpaghettiCan_Opened", 0));
+			LeftoverItems.Insert(new LeftoverItem("UnknownFoodCan_Opened", "Empty_UnknownFoodCan_Opened", 0));
+			LeftoverItems.Insert(new LeftoverItem("SardinesCan_Opened", "Empty_SardinesCan_Opened", 0));
+			LeftoverItems.Insert(new LeftoverItem("TunaCan_Opened", "Empty_TunaCan_Opened", 0));
+			LeftoverItems.Insert(new LeftoverItem("DogFoodCan_Opened", "Empty_DogFoodCan_Opened", 0));
+			LeftoverItems.Insert(new LeftoverItem("CatFoodCan_Opened", "Empty_CatFoodCan_Opened", 0));
+			LeftoverItems.Insert(new LeftoverItem("PorkCan_Opened", "Empty_PorkCan_Opened", 0));
+			LeftoverItems.Insert(new LeftoverItem("Lunchmeat_Opened", "Empty_Lunchmeat_Opened", 0));
+			LeftoverItems.Insert(new LeftoverItem("Pate_Opened", "Empty_Pate_Opened", 0));
+			LeftoverItems.Insert(new LeftoverItem("Pajka_Opened", "Empty_Pate_Opened", 0));
+			LeftoverItems.Insert(new LeftoverItem("BrisketSpread_Opened", "Empty_BrisketSpread_Opened", 0));
+			LeftoverItems.Insert(new LeftoverItem("BloodBagIV", "Used_BloodBagIV", 1, true));
+			LeftoverItems.Insert(new LeftoverItem("SalineBagIV", "Used_SalineBagIV", 1, true));
+			LeftoverItems.Insert(new LeftoverItem("Morphine", "Used_Morphine", 1, true));
+			LeftoverItems.Insert(new LeftoverItem("Epinephrine", "Used_Epinephrine", 1, true));
+			LeftoverItems.Insert(new LeftoverItem("AntiChemInjector", "Used_AntiChemInjector", 1, true));
+			// Example modded items
+			LeftoverItems.Insert(new LeftoverItem("MassMREPB", "MassMREPB", 1));
+			LeftoverItems.Insert(new LeftoverItem("MassMREJelly", "MassMREJelly", 1));
+			LeftoverItems.Insert(new LeftoverItem("MassMREGrapeJelly", "MassMREGrapeJelly", 1));
+			LeftoverItems.Insert(new LeftoverItem("MassMRETobasco", "MassMRETobasco", 1));
+			LeftoverItems.Insert(new LeftoverItem("MassMREHotCheeseSpread", "MassMREHotCheeseSpread", 1));
+			LeftoverItems.Insert(new LeftoverItem("MassMREDBBPudding", "MassMREDBBPudding", 1));
+			LeftoverItems.Insert(new LeftoverItem("MassMREDBBCobbler", "MassMREDBBCobbler", 1));
+			LeftoverItems.Insert(new LeftoverItem("MassMREMCSpaghetti", "MassMREMCSpaghetti", 1));
+			LeftoverItems.Insert(new LeftoverItem("MassMREMCBeefTaco", "MassMREMCBeefTaco", 1));
+			LeftoverItems.Insert(new LeftoverItem("MassMREMCChilliMac", "MassMREMCChilliMac", 1));
+			LeftoverItems.Insert(new LeftoverItem("MassMREMCBeefStew", "MassMREMCBeefStew", 1));
+			LeftoverItems.Insert(new LeftoverItem("MassMRETortillaW", "MassMRETortillaW", 1));
+			// Save config
+			Save();
 		}
 	}
 
